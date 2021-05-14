7bb9a84a17bd8e95daf1b3c6c72849c8e93986f3
AurorArctic <aurora.maria.tumminello.z@gmail.com>
2021-05-14 19:08:47 +0200

    ResNet152

---
+++ SOL3_resnet152/functions.py
@@ -0,0 +1,54 @@
+# import the necessary packages
+
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2' 
+
+import glob
+import pandas as pd
+import numpy as np
+from tqdm import tqdm
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.resnet50 import preprocess_input
+
+
+"""
+Query of a single image
+"""
+def queryImage(img_path, img_size, model_resnet50, model_neighbors, filenames):
+    input_shape = (img_size, img_size, 3)
+    img = image.load_img(img_path, target_size=(input_shape[0], input_shape[1]))
+    img_array = image.img_to_array(img)
+    expanded_img_array = np.expand_dims(img_array, axis=0)
+    preprocessed_img = preprocess_input(expanded_img_array)
+
+    test_img_features = model_resnet50.predict(preprocessed_img, batch_size=1)
+    _, indices = model_neighbors.kneighbors(test_img_features)
+
+    pred_names = []
+    for index in indices[0]:
+        pred_names.append(cleanName(filenames[index]))
+
+    return pred_names
+
+
+"""
+Query of all images in a given directory
+"""
+def queryImageAll(query_path, img_size, model_resnet50, model_neighbors, filenames,res_filename):
+
+    res_list = []
+    for img_path in tqdm(glob.glob(query_path + "/*.jpg")):
+        tempRes = queryImage(img_path, img_size, model_resnet50, model_neighbors, filenames)
+        res_list.append( [cleanName(img_path)] + tempRes)
+
+    res_df = pd.DataFrame(res_list, columns = ["Query", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"])
+    res_df.to_csv(res_filename, index=False)
+    return res_df
+
+
+"""
+Given a path, it terurns ony the name of a .jpg file
+"""
+def cleanName(path):
+    start = path.rfind('\\') +1
+    return path[start:-4]
--- SOL3_resnet50/final_script.py
+++ SOL3_resnet50/final_script.py
@@ -1,11 +1,16 @@
 # import the necessary packages
-from functions import *
 
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2' 
 
-import joblib as pickle
-from tensorflow import keras
+from sklearn.neighbors import NearestNeighbors
+from tensorflow.keras.applications.resnet50 import ResNet50, preprocess_input
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from functions import *
+
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
 
 print("\n===========================================\n")
 
@@ -15,30 +20,53 @@ print("> Libraries Imported")
 SETTINGS
 """
 
+gallery_path = "../dataset/validation"
 query_path = "../dataset/validation/query"
 setup_dir_path = "setup_files"
+
+# ResNet Settings
 img_size = 256
+pooling = "avg"
+num_epochs = 20
+
+# ImageDataGenerator Settings
+batch_size = 128
+
+# NearestNeighbors Settings
+algorithm_for_NN = "ball_tree"
+metric_for_NN = "euclidean"
 
 """
 SCRIPT
 """
 
-print("\n> Loading all needed files for querying")
+# Create `Resnet50` model without top layers so we get convolutional features as output instead of the image class probability.
 
-# Import ResNet model, NearestNeighbors model and filenames
-model_resnet50_path = setup_dir_path + "/model_resnet50.sav"
-model_neighbors_path = setup_dir_path + "/model_neighbors.sav"
-filenames_index_path = setup_dir_path + "/filenames_index.sav"
+model = ResNet50(weights="imagenet", include_top=False,
+                 input_shape=(img_size, img_size, 3), pooling=pooling)
+print("\n> Model ResNet50 Imported")
 
-model_resnet50 = keras.models.load_model(model_resnet50_path)
-model_neighbors = pickle.load(open(model_neighbors_path, 'rb'))
-filenames = pickle.load(open(filenames_index_path, 'rb'))
+img_gen = ImageDataGenerator(preprocessing_function=preprocess_input)
 
-print("> All setup files loaded")
+datagen = img_gen.flow_from_directory(gallery_path,
+                                      classes=['gallery'],
+                                      target_size=(img_size, img_size),
+                                      batch_size=batch_size,
+                                      class_mode=None,
+                                      shuffle=False)
+
+feature_list = model.predict(datagen, num_epochs)
+
+# Get full path for all the images in our dataset
+filenames = [gallery_path + "/" + s for s in datagen.filenames]
+
+print("\n> Fitting Nearest Neighbor Algorithm")
+neighbors = NearestNeighbors(n_neighbors=10,
+                             algorithm=algorithm_for_NN,
+                             metric=metric_for_NN)
+neighbors.fit(feature_list)
 
 # Execute query predictions
 print("\n> Executing Query")
-
-queryImageAll(query_path, img_size, model_resnet50, model_neighbors, filenames)
-
+queryImageAll(query_path, img_size, model, neighbors, filenames)
 print("\n> Results correctly stored")
