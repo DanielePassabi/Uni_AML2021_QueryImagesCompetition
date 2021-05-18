dcf1956fbfa15052999df0020518bbe8842331eb
Daniele <38936531+DanielePassabi@users.noreply.github.com>
2021-05-18 10:24:36 +0200

    new distractors added to training and training augmented

--- dataset/rename.py
+++ dataset/rename.py
@@ -1,12 +1,14 @@
 #%%
 import os
-path = 'C:/Users/auror/OneDrive/Documenti/GitHub/AML_Competition_2021/dataset/new_data/'
+path = 'C:/Users/danyp/Documents/Programmazione/AML_Competition_2021/AML_Competition_2021/dataset/temp/'
 files = os.listdir(path)
 #%%
+
+current_distractors_number = 172
 
 #%%
 for index,file in enumerate(files):
     temp_path = path+file+"/"
     for i,f in enumerate(os.listdir(temp_path)):
-        os.rename(os.path.join(temp_path, f), os.path.join(temp_path, str(i).join([file,'.jpg'])))
+        os.rename(os.path.join(temp_path, f), os.path.join(temp_path, str(i+current_distractors_number).join([file,'.jpg'])))
 # %%
